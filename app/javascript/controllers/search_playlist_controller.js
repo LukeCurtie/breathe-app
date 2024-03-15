f9f891a98a4ba656ceafb5ea147bd1cdf953446e
luke curtis <curtisluke67@gmail.com>
2024-03-15 16:05:41 +0400

    spotify and chat

--- app/javascript/controllers/search_playlist_controller.js
+++ app/javascript/controllers/search_playlist_controller.js
@@ -1,32 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["form", "input", "result", "loading"];
+  static targets = ["form", "input", "result", "searchIcon"];
 
   connect() {
-    this.loadingTarget.style.display = 'none';
+    this.inputTarget.addEventListener('focus', this.hideSearchIcon.bind(this));
+    this.inputTarget.addEventListener('blur', this.showSearchIcon.bind(this));
   }
 
   update(e) {
     e.preventDefault();
-
-    this.loadingTarget.style.display = 'block';
 
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`;
     fetch(url, { headers: { "Accept": "text/plain" } })
       .then((response) => response.text())
       .then((data) => {
         this.resultTarget.outerHTML = data;
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      })
-      .finally(() => {
-        this.loadingTarget.style.display = 'none';
       });
   }
 
   clear() {
     this.inputTarget.value = "";
+  }
+
+  hideSearchIcon() {
+    this.searchIconTarget.style.display = 'none';
+  }
+
+  showSearchIcon() {
+    this.searchIconTarget.style.display = 'block';
   }
 }
