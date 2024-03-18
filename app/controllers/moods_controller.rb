6ac9148f1946f881b8564e1cebd0652dd00cf0c2
sanjivaniramburn@gmail.com <sanjivaniramburn@gmail.com>
2024-03-18 16:13:59 +0400

    debug

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,7 +1,7 @@
 class MoodsController < ApplicationController
   def index
     start_date = params.fetch(:date, Date.today).to_date
-    @moods = Mood.where(date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @moods = Mood.where(date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week, user: current_user)
   end
 
   def create
