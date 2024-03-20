bd76fe2324e1eb5db25de79b85c38741d3f1c5ba
luke curtis <curtisluke67@gmail.com>
2024-03-20 21:39:40 +0400

    chat room

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number, name: String}
   static targets = ["messages"]
 
   connect() {
@@ -17,9 +17,31 @@ export default class extends Controller {
   }
 
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML('beforeend', data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message, data.name)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "position-right" : "position-left"
+  }
+
+  #justifyClassSender(currentUserIsSender) {
+    return currentUserIsSender ? "sender" : "receiver"
+  }
+
+  #buildMessageElement(currentUserIsSender, message, name) {
+    console.log(message)
+    return `
+      <div class=${this.#justifyClass(currentUserIsSender)}>
+        <div class="message-box ${this.#justifyClassSender(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
   resetForm(event) {
     event.target.reset()
   }
